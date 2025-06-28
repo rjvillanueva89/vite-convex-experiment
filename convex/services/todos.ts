ff34c3edfd74f7a0905ebf7b33d0bfb57d5b493b
RJ Villanueva <rj.villanueva89@gmail.com>
2025-06-28 17:28:01 +0800

    feat: add createTodo mutation for inserting new todo items

--- convex/services/todos.ts
+++ convex/services/todos.ts
@@ -1,8 +1,21 @@
-import { query } from '../_generated/server'
+import { v } from 'convex/values'
+import { mutation, query } from '../_generated/server'
 
 export const getTodos = query({
   args: {},
   handler: async (ctx) => {
     return await ctx.db.query('todos').collect()
+  },
+})
+
+export const createTodo = mutation({
+  args: {
+    title: v.string(),
+  },
+  handler: async (ctx, args) => {
+    return await ctx.db.insert('todos', {
+      title: args.title,
+      isCompleted: false,
+    })
   },
 })
