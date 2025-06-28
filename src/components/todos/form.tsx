1e61673a08d6371365448b96bacfca623c0485d4
RJ Villanueva <rj.villanueva89@gmail.com>
2025-06-28 18:18:47 +0800

    feat: implement todo list UI with Convex data fetching and mutations

--- src/components/todos/form.tsx
+++ src/components/todos/form.tsx
@@ -1,12 +1,27 @@
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
+import { api } from '@convex/api'
+import { useMutation } from 'convex/react'
 import { PlusIcon } from 'lucide-react'
+import { useState } from 'react'
 
 export const TodoForm = () => {
+  const [title, setTitle] = useState('')
+  const createTodo = useMutation(api.services.todos.createTodo)
+
+  const handleClick = () => {
+    createTodo({ title })
+    setTitle('')
+  }
+
   return (
     <div className="flex gap-2">
-      <Input placeholder="Add a todo..." />
-      <Button type="button">
+      <Input
+        placeholder="Add a todo..."
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
+      <Button type="button" onClick={handleClick}>
         <PlusIcon />
       </Button>
     </div>
--- src/components/todos/list.tsx
+++ src/components/todos/list.tsx
@@ -1,7 +1,23 @@
+import { api } from '@convex/api'
+import type { Doc } from '@convex/dataModel'
+import { useQuery } from 'convex/react'
+import { SquareIcon } from 'lucide-react'
+
+type Todo = Doc<'todos'>
+
 export const TodosList = () => {
+  const todos = useQuery(api.services.todos.getTodos)
   return (
-    <div>
-      <h1>Todo List</h1>
-    </div>
+    <ul className="divide-y divide-dashed">
+      {todos?.map((todo) => <TodoItem key={todo._id} todo={todo} />)}
+    </ul>
+  )
+}
+
+const TodoItem = ({ todo }: { todo: Todo }) => {
+  return (
+    <li key={todo._id} className="py-2 flex gap-2 items-center">
+      <SquareIcon className="size-4" /> {todo.title}
+    </li>
   )
 }
